4e3057e276c4de524092e9f06f7625947d1577b6
FathulNisaAini <92559540+FathulNisaAini@users.noreply.github.com>
2022-06-21 19:23:05 +0700

    Create AdminController.php

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Tiket;
+
+
+class AdminController extends Controller
+{
+    public function index()
+    {      
+        return view('admin');
+    }
+}
