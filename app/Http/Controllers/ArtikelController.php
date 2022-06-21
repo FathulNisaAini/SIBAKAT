4d77fa565b453f7c208f1ef8d397f68f6d5b9d6f
FathulNisaAini <92559540+FathulNisaAini@users.noreply.github.com>
2022-06-21 19:23:09 +0700

    Create ArtikelController.php

---
+++ app/Http/Controllers/ArtikelController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Tiket;
+
+
+class ArtikelController extends Controller
+{
+    public function index()
+    {      
+        return view('artikel');
+    }
+}
